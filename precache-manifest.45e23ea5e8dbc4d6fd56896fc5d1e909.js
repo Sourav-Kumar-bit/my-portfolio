93b52a4f9c3ffafacf088438f426be43e44a1900
Sourav Kumar <souravkr.62@gmail.com>
2022-10-23 18:34:36 +0530

    Updates

---
+++ precache-manifest.45e23ea5e8dbc4d6fd56896fc5d1e909.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "11db8a8cee33ace4dd73",
+    "url": "/my-portfolio/static/css/main.9a0a197a.chunk.css"
+  },
+  {
+    "revision": "11db8a8cee33ace4dd73",
+    "url": "/my-portfolio/static/js/main.1b0af2e3.chunk.js"
+  },
+  {
+    "revision": "7715b3c223a96b7e4ae0",
+    "url": "/my-portfolio/static/js/runtime~main.041f7f8b.js"
+  },
+  {
+    "revision": "9aeb447654b00cfd154a",
+    "url": "/my-portfolio/static/css/2.a70358ab.chunk.css"
+  },
+  {
+    "revision": "9aeb447654b00cfd154a",
+    "url": "/my-portfolio/static/js/2.e15709b0.chunk.js"
+  },
+  {
+    "revision": "31b6cf34cc0d3bd4bc29",
+    "url": "/my-portfolio/static/js/3.bf2e35d4.chunk.js"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/my-portfolio/static/media/linkedinIcon.dff2b624.svg"
+  },
+  {
+    "revision": "13390fa348de0767da94798b066d3f94",
+    "url": "/my-portfolio/static/media/githubIcon.13390fa3.svg"
+  },
+  {
+    "revision": "e28d5d953d4d0cc66e074c1a6285ef2b",
+    "url": "/my-portfolio/static/media/My Resume.e28d5d95.pdf"
+  },
+  {
+    "revision": "4aaf7b6890530471fb1fdd02b1c6f6ef",
+    "url": "/my-portfolio/static/media/home-main1.4aaf7b68.svg"
+  },
+  {
+    "revision": "a4a99db8ddbc7c43c12473aad284624b",
+    "url": "/my-portfolio/static/media/AvatarMaker.a4a99db8.svg"
+  },
+  {
+    "revision": "f67c34cfe7bd4c1ef97420fc3f2325f8",
+    "url": "/my-portfolio/static/media/robot.f67c34cf.png"
+  },
+  {
+    "revision": "a22e3a98555b561fbfa3e68f93366fe6",
+    "url": "/my-portfolio/static/media/frontend.a22e3a98.png"
+  },
+  {
+    "revision": "0b1f3c20e1cd5cb2f1b7756a96f37822",
+    "url": "/my-portfolio/static/media/dual.0b1f3c20.png"
+  },
+  {
+    "revision": "405b04cc7f6be9abded010580d3d046b",
+    "url": "/my-portfolio/static/media/web design.405b04cc.jpg"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/my-portfolio/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "17fcbbbd4028e1783891b6c62b034ed4",
+    "url": "/my-portfolio/static/media/contact-img2.17fcbbbd.png"
+  },
+  {
+    "revision": "a29719b87792f87b897473bcf0be4d46",
+    "url": "/my-portfolio/static/media/spinning-circles.a29719b8.svg"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/my-portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/my-portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/my-portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "832974d1a8369962b0fdb3077b820961",
+    "url": "/my-portfolio/static/media/sky5.832974d1.jpg"
+  },
+  {
+    "revision": "0d8444e66134b04fc083ed1dd1f2022f",
+    "url": "/my-portfolio/static/media/sky10.0d8444e6.jpg"
+  },
+  {
+    "revision": "f17438d8c18b4074d997e9754a3bbca8",
+    "url": "/my-portfolio/static/media/sky7.f17438d8.jpg"
+  },
+  {
+    "revision": "06dbfcf6e42f55a5ce8c59ec9a3a733b",
+    "url": "/my-portfolio/index.html"
+  }
+];
